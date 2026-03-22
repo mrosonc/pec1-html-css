3e1370f47a0f2e11e850215095e9c985fa90bb42
Marcos Rosón Calvo <mrosonc@uoc.edu>
2026-03-22 10:51:27 +0100

    Aplicar mejoras de accesibilidad y ajustes visuales

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
-console.log("Proyecto arrancado correctamente");
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "aos/dist/aos.css";
+import AOS from "aos";
+
+AOS.init({
+    duration: 700,
+    easing: "ease-out",
+    once: true
+});
